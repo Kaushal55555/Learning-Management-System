55416c0045e78c8c432e1297de65112eb0ff5328
Kaushal55555 <lordqshal@gmail.com>
2025-09-16 14:11:16 +0545

    handling server activities

---
+++ server/index.js
@@ -0,0 +1,27 @@
+import express from "express";
+import dotenv from "dotenv";
+import connectDB from "./database/db.js";
+import userRoute from "./routes/user.route.js";
+
+dotenv.config({});
+
+// call database connection function here
+connectDB();
+
+const app = express();
+
+const PORT = process.env.PORT || 5000;
+
+// APIs
+app.use("/api/v1/user", userRoute);
+
+app.get("/home", (_,res) => {
+    res.status(200).json({
+        success: true,
+        message: "Hello i am coming from backend"
+    })
+})
+
+app.listen(PORT, () => {
+  console.log(`Server listen at port ${PORT}`);
+})
